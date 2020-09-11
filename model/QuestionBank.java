38479b4016cd5edc7e48a622cbcf0110c1030822
Vinu <vinudn@yahoo.com>
2020-09-10 19:52:09 -0500

    Choices.

--- model/QuestionBank.java
+++ model/QuestionBank.java
@@ -21,6 +21,36 @@ public class QuestionBank {
 
 
 	));
+	var c1 = new ChoiceQuestion(
+		"What is the captail city of Oklahoma?",
+		"2"
+
+	);
+		c1.addChoice("Norman");
+		c1.addChoice("Oklahoma City");
+		c1.addChoice("USA");
+		c1.addChoice("Tulsa");
+		database.add(c1);
+
+		var c2 = new ChoiceQuestion(
+			
+		 "Which of the foloowing is not a Java Keyword?",
+		"4"
+		);
+		c2.addChoice("int");
+		c2.addChoice("for");
+		c2.addChoice("instanceof");
+		c2.addChoice("String");
+		database.add(c2);
+
+	
+
+
+
+
+
+
+
 	}
 
 
--- test/QuestionTest.java
+++ test/QuestionTest.java
@@ -11,13 +11,27 @@ public class QuestionTest {
 
 		ArrayList<Question> db = QuestionBank.database;
 		Question q1 =db.get(0);
-		assert q1.checkAnswer("Bill Gates") == false;
-		assert q1.checkAnswer("James Gosling") == true;
-		assert q1.checkAnswer("james gosling") == true;
+		q1.display();
+		assert !q1.checkAnswer("Bill Gates");//== false;
+		assert q1.checkAnswer("James Gosling");// == true;
+		assert q1.checkAnswer("james gosling"); //== true;
+
+	Question q2 =db.get(1);
+	q2.display();
+	assert q2.checkAnswer("Edmond");
+	assert !q2.checkAnswer("Tusla");
+
+	Question q3 =db.get(2);
+	q3.display();
+	assert q3.checkAnswer("2");
+	assert!q3.checkAnswer("3");
 
 	
-	
-	
+	Question q4 =db.get(3);
+	q4.display();
+	assert q4.checkAnswer("4");
+	assert !q4.checkAnswer("2");
+
 	}
 	
 }
